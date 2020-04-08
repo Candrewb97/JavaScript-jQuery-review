cd6835faa9d4e4419792d97be5f94f07abb1775b
Caleb Bridges <calebbridges97@gmail.com>
2020-04-08 10:36:09 -0400

    sandwhich calculator and pizza array

---
+++ Pizza.js
@@ -0,0 +1,11 @@
+/*Exercise - Pizza
+1. Create an array of pizza toppings
+2. Write a function that we can call
+3. Function should return a string that describes the pizza, example “A delicious
+pizza with Cheese and Pepperoni and Garlic and Peppers.”
+*/
+
+var pizza = ["cheese", "pepperoni", "sausage", "peppers", "mushrooms"]
+console.log('A delicious pizza with',pizza.join());
+
+
